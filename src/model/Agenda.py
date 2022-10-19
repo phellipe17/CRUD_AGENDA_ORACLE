aa1c9763b3134255430437cf992cf6bf93a6f54e
phellipe17 <phellipess_17@hotmail.com>
2022-10-19 16:10:29 -0300

    reparado o diagrama e o model agenda com atributos faltando

--- src/model/Agenda.py
+++ src/model/Agenda.py
@@ -1,16 +1,23 @@
 class Agenda:
 
-    def __init__(self,nomepessoa:str, id_pessoa:int, endereco:str,telefone:str, e_mail:str ):
+    def __init__(self,id_agenda:int,cpf:str,nomepessoa:str, id_pessoa:int, endereco:str,telefone:str, e_mail:str ):
+        self.setId_agenda(id_agenda)
+        self.setCpf(cpf)
         self.setNomepessoa(nomepessoa)
         self.setId_pessoa(id_pessoa)
         self.set_endereco(endereco)
         self.set_telefone(telefone)
         self.setE_mail(e_mail)
 
+    def setId_agenda(self,id_agenda:int):
+        self.id_agenda=id_agenda
+
+    def setCpf(self,cpf:str):
+        self.cpf=cpf
+
     def set_endereco(self, endereco:str):
         self.endereco=endereco
     
-
     def set_telefone(self, telefone:str):
         self.telefone=telefone
 
@@ -20,13 +27,18 @@ class Agenda:
     def setE_mail(self, e_mail:str):
         self.e_mail=e_mail
     
-    def setNomePEssoa(self,nomepessoa):
+    def setNomepessoa(self,nomepessoa):
         self.nomepessoa=nomepessoa
+
+    def getId_agenda(self)->int:
+        return self.id_agenda
+    
+    def getCpf(self)->str:
+        return self.cpf
 
     def get_Endereco(self)->str:
         return self.endereco
      
-
     def getTelefone(self)->str:
         return self.telefone
 
